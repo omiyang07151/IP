551c5feb754c26e2d739dc9f56372f1d72d88047
omiyang <22418066@qq.com>
2016-09-29 16:48:32 +0800

    update

---
+++ Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -0,0 +1,16 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+</head>
+<body>
+
+======= Layout =======
+
+index/index
+
+
+======= Layout =======
+</body>
+</html>
---
+++ Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,15 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+
+======= Layout =======
+
+Home/index
+
+======= Layout =======
+</body>
+</html>
