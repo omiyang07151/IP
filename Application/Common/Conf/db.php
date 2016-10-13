25c164753f000404f7f39537dc7fa903b0039092
omiyang <22418066@qq.com>
2016-10-13 14:35:33 +0800

    update

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -8,10 +8,10 @@ else {
 $config = array(
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
+    'DB_HOST'   => '120.26.199.234', // 服务器地址
     'DB_NAME'   => 'ip', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => 'Enjoy@Touch#2015',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => '', // 数据库表前缀
 );
