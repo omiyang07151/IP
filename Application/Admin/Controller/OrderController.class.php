72c09fb8e5dcf79b480256210242be9bfa2babb3
omiyang <22418066@qq.com>
2016-10-22 00:35:28 +0800

    添加订单明细

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -16,4 +16,23 @@ class OrderController extends AdminBaseController {
 //        pr($data, true);
         $this->jsonpajax($data);
     }
+
+    public function detail(){
+
+        $id = I('id');
+        $orderModel = D('OrderMst');
+
+        $order = $orderModel->getById($id);
+        $order['checkin_info'] = json_decode($order['checkin_info'], true);
+
+        $productModel = D('ProductMst');
+        $order['product'] = $productModel->getProductById($order['product_mst_id']);
+
+        $userModel = D('UserMst');
+        $order['user'] = $userModel->getById($order['user_mst_id']);
+
+        $this->assign('order', $order);
+
+        $this->display();
+    }
 }
